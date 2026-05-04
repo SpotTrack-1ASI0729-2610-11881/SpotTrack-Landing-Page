68b1da4055a611829b29d92167af06249b104fe1
Llummo <167897805+Llummo@users.noreply.github.com>
2026-05-03 19:02:59 -0500

    chore: add translate service

---
+++ src/app/components/pricing-section/pricing-section.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pricing-section',
+  imports: [],
+  templateUrl: './pricing-section.html',
+  styleUrl: './pricing-section.css',
+})
+export class PricingSection {}
