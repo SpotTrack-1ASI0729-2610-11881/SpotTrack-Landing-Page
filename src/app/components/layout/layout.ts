c5e629fcee3f5adb7c754a7f5fb2dd1ffee78116
jcus1510 <167936372+jcus1510@users.noreply.github.com>
2026-05-05 22:49:01 -0500

    feat:add-contact

--- src/app/components/layout/layout.ts
+++ src/app/components/layout/layout.ts
@@ -1,10 +1,11 @@
     import { Component } from '@angular/core';
-import { HeroSectionComponent } from '../hero-section/hero-section.component';
+    import { HeroSectionComponent } from '../hero-section/hero-section.component';
     import { PricingSection } from '../pricing-section/pricing-section';
+    import { ContactComponent } from '../contact-section/contact-section.component';
 
 @Component({
   selector: 'app-layout',
-  imports: [HeroSectionComponent, PricingSection],
+  imports: [HeroSectionComponent, PricingSection,ContactComponent],
   templateUrl: './layout.html',
   styleUrl: './layout.css',
 })
